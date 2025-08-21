5b479bc97be1efb64f987fc31bb3b0a6ba9f3b24
Roop <roop56u@gmail.com>
2025-08-21 06:28:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -33,8 +33,10 @@ const RadioGroupItem = React.forwardRef<
       )}
       {...props}
     >
-      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="h-2.5 w-2.5 fill-current text-current" />
+      <RadioGroupPrimitive.Indicator>
+        <div className="flex items-center justify-center">
+          <Circle className="h-2.5 w-2.5 fill-current text-current" />
+        </div>
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
