a524acd48824c4ea032cc7c690bf15d550951e12
Roop <roop56u@gmail.com>
2025-08-23 18:43:14 +0000

    I think you should try zoom in view but make sure adjust all panelsshoul

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -37,7 +37,7 @@ const CardTitle = React.forwardRef<
   <h3
     ref={ref}
     className={cn(
-      "text-base font-semibold leading-none tracking-tight",
+      "text-lg font-semibold leading-none tracking-tight",
       className
     )}
     {...props}
@@ -51,7 +51,7 @@ const CardDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <p
     ref={ref}
-    className={cn("text-xs text-muted-foreground", className)}
+    className={cn("text-sm text-muted-foreground", className)}
     {...props}
   />
 ))
